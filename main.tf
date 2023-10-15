65abeca0b024635b21d3acb97fa12df16d08f881
Spasina Spasova <75899012+SpasinaSpasova@users.noreply.github.com>
2023-10-15 11:56:06 +0300

    ed

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "azurerm_mssql_firewall_rule" "firewall" {
 
 resource "azurerm_app_service_source_control" "example" {
   app_id                 = azurerm_linux_web_app.alwa.id
-  repo_url               = "https://github.com/dimosoftuni/taskboard"
+  repo_url               = "https://github.com/SpasinaSpasova/TaskBoard"
   branch                 = "main"
   use_manual_integration = true
 }
